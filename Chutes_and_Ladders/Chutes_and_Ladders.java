0cb0ac77648e0c8eb0d5fb48dc225638f9dd2149
jaapfel <j_apfel@u.pacific.edu>
2015-06-21 17:17:22 -0700

    Fixed Ry's lack of constructor.

--- Chutes_and_Ladders/Chutes_and_Ladders.java
+++ Chutes_and_Ladders/Chutes_and_Ladders.java
@@ -1,16 +1,18 @@
+import javax.swing.*;
 
 public class Chutes_and_Ladders {
 	static final int GRID_WIDTH = 10;
 	static final int GRID_HEIGHT = 10;
 	private class Board {
 		private Tile[][] board = new Tile[GRID_WIDTH][GRID_HEIGHT];
-		public void Board() {
+		public Board() {
 			for(int i = 0; i < GRID_HEIGHT; i++) {
 				for(int j = 0; j < GRID_WIDTH; j++) {
 					board[i][j] = new Tile(i * 10 + j);
 				}
 			}
 		}
+		
 
 		private class Tile {
 			int destination; // The terminal point of the "chute" or "ladder". Matches index if self.
